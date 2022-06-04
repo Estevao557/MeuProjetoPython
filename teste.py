56ab184e8f9bb5632e67bbc81365521a14c3c7c0
unknown <rat871@hotmail.com>
2022-06-04 08:36:50 -0300

    Inserido Nova linha

--- teste.py
+++ teste.py
@@ -3,9 +3,10 @@ print("Alô Mundo")
 print("Linha 2")
 print("Estêvão Martins Fidelis")
 print("ADICIONADO 4 LINHA")
+print("Nova Linha" )
 
 
 valor=10
 if valor <= 10:
-	print("valor")
+	print("teste")
 	
