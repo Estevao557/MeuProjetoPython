10bce6e0a457f861940ce63e800885fa1e006452
Estêvão557 <rat871@hotmail.com>
2022-06-04 09:09:41 -0300

    Teste Js

---
+++ Untitled-1.js
@@ -0,0 +1,13 @@
+//  tipo Number
+
+const  meuNumero = 3 ;
+
+const primeiroNumero = 2;
+const segundoNumero = 3;
+const terceiroNumero = 6
+
+const operacaoMatematica = primeiroNumero + segundoNumero + terceiroNumero;
+
+const operacaoMatematica2 = primeiroNumero * segundoNumero - terceiroNumero;
+
+console.log(operacaoMatematica2)
